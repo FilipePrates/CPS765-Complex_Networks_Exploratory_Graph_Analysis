407798aba1c2146457a0e9cd424649fd77584d43
FilipePrates <filipeprates@poli.ufrj.br>
2023-10-27 14:44:12 -0300

    adapting the code for data

---
+++ README.md
@@ -0,0 +1,21 @@
+Kaggle chess players (2010)
+Social
+Offline
+Description
+A network among chess players (nodes) giving the chess match outcomes (edges), for game-by-game results among the world’s top chess players. The direction of edge (i,j) denotes white player (i) and black player (j). Each edge is timestamped (approximate). Edge sign is +1 for a win by white, 0 for draw, and -1 for a win by black.
+Network summary
+Edge Type
+Game
+Node Type
+Player
+Graph properties
+Directed, Signed, Timestamps
+Citation
+Kaggle, "Chess ratings - Elo versus the Rest of the World." https://www.kaggle.com/c/chess/data (2010). Link
+Hosted by
+Data hosted by Netzschleuder
+Suggested by
+Telly
+Network data
+Name Nodes Edges File Size File Type Format Source
+kaggleChess2010 7301 65053 266.74kb txt edgelist Link
--- igraphHW.py
+++ igraphHW.py
@@ -3,23 +3,11 @@ import matplotlib.pyplot as plt
 
 edges = []
 
-def main():
-
-    # Construct a graph with 5 vertices
-    n_vertices = 5
-    edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 4)]
+def main(n_vertices, edges, weights):
     g = ig.Graph(n_vertices, edges)
-
     # Set attributes for the graph, nodes, and edges
-    g["title"] = "Small Social Network"
-    g.vs["name"] = ["Daniel Morillas", "Kathy Archer", "Kyle Ding", "Joshua Walton", "Jana Hoyer"]
-    g.vs["gender"] = ["M", "F", "F", "M", "F"]
-    g.es["married"] = [False, False, False, False, False, False, False, True]
-
-    # Set individual attributes
-    g.vs[1]["name"] = "Kathy Morillas"
-    g.es[0]["married"] = True
-
+    g["title"] = "Chess Games"
+    g.es["weights"] = weights
     # Plot in matplotlib
     # Note that attributes can be set globally (e.g. vertex_size), or set individually using arrays (e.g. vertex_color)
     fig, ax = plt.subplots(figsize=(5,5))
@@ -28,37 +16,39 @@ def main():
         target=ax,
         layout="circle", # print nodes in a circular layout
         vertex_size=0.1,
-        vertex_color=["steelblue" if gender == "M" else "salmon" for gender in g.vs["gender"]],
+        vertex_color=["steelblue"],
         vertex_frame_width=4.0,
         vertex_frame_color="white",
-        vertex_label=g.vs["name"],
+        vertex_label=g.vs.indices,
         vertex_label_size=7.0,
-        edge_width=[2 if married else 1 for married in g.es["married"]],
-        edge_color=["#7142cf" if married else "#AAA" for married in g.es["married"]]
+        edge_color=["#7142cf" if win == "1" else "salmon" for win in g.es["weights"]]
     )
 
     plt.show()
 
     # Save the graph as an image file
-    fig.savefig('social_network.png')
-    fig.savefig('social_network.jpg')
-    fig.savefig('social_network.pdf')
+    fig.savefig('chess_network.png')
+    fig.savefig('chess_network.jpg')
+    fig.savefig('chess_network.pdf')
 
     # Export and import a graph as a GML file.
-    g.save("social_network.gml")
-    g = ig.load("social_network.gml")
+    g.save("chess_network.gml")
+    g = ig.load("chess_network.gml")
 
 def readGraph():
     f = open("network_db/download.tsv.chess/chess/out.chess", "r")
-    edges = []
     print(f.readline()) # cabeçalho
+    edges = []
+    weights = []
+    aux = 1
     for line in f:
-        # edges = [(0, 1), (0, 2), (0, 3), (0, 4), (1, 2), (1, 3), (1, 4), (3, 4)]
-        print(line)
-        line = line.split('\t')[0].split(" ")
-        edges.append((int(line[0]), int(line[1]), int(line[2])))
+        aux += 1
+        if(aux < 40):
+            line = line.split('\t')[0].split(" ")
+            edges.append((int(line[0]), int(line[1])))
+            weights.append(int(line[2]))
     num_vertices = findMax(edges)
-    return num_vertices, edges
+    return num_vertices, edges, weights
 
 def findMax(arr):
     max_value = 0
@@ -70,6 +60,5 @@ def findMax(arr):
             max_value = tuple[1]
     return max_value
 
-num_vertices, edges = readGraph()
-
-print(num_vertices)
+num_vertices, edges, weights = readGraph()
+main(num_vertices, edges, weights)
